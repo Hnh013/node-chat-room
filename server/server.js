e17efc514ca64b08b7dd7fdce1e26959b7f94e25
Harshit Negi <najay357@gmail.com>
2019-05-30 21:01:46 +0530

    last error removed

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
     
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
+    //socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
     
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
     
